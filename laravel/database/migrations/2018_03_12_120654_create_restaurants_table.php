b0780809cc0e126e40b992050df565c547f012b2
Nitesh Sharma <nsniteshsharma47@gmail.com>
2018-03-30 06:10:11 +0530

    Init laravel

---
+++ laravel/database/migrations/2018_03_12_120654_create_restaurants_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('logo');
+            $table->string('image');
+            $table->integer('phone');
+            $table->string('addr_one');
+            $table->string('addr_two');
+            $table->integer('pincode');
+            $table->string('gst_no');
+            $table->timestamps();
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
---
+++ laravel/database/migrations/2018_03_12_121508_create_categories_table.php
@@ -0,0 +1,38 @@
+\<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('image');
+            $table->timestamps();
+
+
+            
+        $table->integer('rest_id')->unsigned();
+        $table->foreign('rest_id')->references('id')->on('restaurants');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
